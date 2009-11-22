aeb68d168002640749394f24d3071b58f1387e4b
jovezhong <jovezhong@gmail.com>
2009-11-22 01:29:43 +0000

    check in the working sample code for douban RESTful service (from Mac OS)

--- kuokuo/test/org/kuokuo/DoubanApiTest.java
+++ kuokuo/test/org/kuokuo/DoubanApiTest.java
@@ -11,21 +11,13 @@ import com.google.gdata.data.douban.SubjectFeed;
 public class DoubanApiTest extends TestCase {
 	public void testSearchMovie() throws Exception {
 		String apiKey = "0fc141a814ca4f3d2f43c4f83e5eedb7";
-		String secret = "530a4522721b9682";
-
-		DoubanService myService = new DoubanService("kuokuo", apiKey, secret);
-		System.out.println("please paste the url in your webbrowser, complete the authorization then come back:");
-		System.out.println(myService.getAuthorizationUrl(null));
-		byte buffer[] = new byte[1];
-		System.in.read(buffer);
+		DoubanService myService = new DoubanService("kuokuo", apiKey);//read-only service
 		
-		myService.getAccessToken();
-		//myService.setAccessToken("", "");
-		
-		SubjectFeed movieFeed = myService.findMovie(null,"钢之炼金术师 2009", 1,2);
+		SubjectFeed movieFeed = myService.findMovie("钢之炼金术师 2009",null, 1,1);
 		List<SubjectEntry> list = movieFeed.getEntries();
 		assertTrue(list.size()>0);
 		SubjectEntry movie = list.get(0);
-		System.out.println(movie.getTitle());
+		System.out.println(movie.getTitle().getPlainText());
+		System.out.println(movie.getLink("image", null).getHref());
 	}
 }
